9433dd920fd5992b9e359c0d8379042a51138930
soulvibescorp <113645673+soulvibescorp@users.noreply.github.com>
2025-04-24 02:38:20 -0400

    Delete SentinelCore-Web-main directory

--- SentinelCore-Web-main/js/modeToggle.js
+++
@@ -1,7 +0,0 @@
-function switchToTerminal() {
-  window.location.href = "terminal.html";
-}
-
-function switchToFuturistic() {
-  window.location.href = "dashboard.html";
-}
--- SentinelCore-Web-main/js/terminalBoot.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const loader = document.getElementById("boot-loader");
-  setTimeout(() => loader.style.display = "none", 1500);
-});
